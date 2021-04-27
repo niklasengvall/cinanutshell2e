4f94a635bbf1c28031ca82a49f9960999331f4c5
Niklas Engvall <niklas_engvall@hotmail.com>
2021-04-28 01:06:44 +0200

    Uppdated the BASH script

--- runcode.sh
+++ runcode.sh
@@ -1,6 +1,7 @@
+# A simple bash script for linux to compile one file and put the executable with .out extension in the output folder
 #!/bin/bash
 echo -e "What file do you want to compile & run, no extension please?" 
 read file
 source="src/${file}.c"
-destination="output/${file}"
+destination="output/${file}.out"
 gcc-10 $source -o $destination -O3 -Wall -std=c17 -Wno-missing-braces
--- rundebug.sh
+++ rundebug.sh
@@ -1,7 +1,9 @@
+# A simple bash script for linux to compile one file and put the executable with .out extension in the output folder
+# and start debugging the file with GDB
 #!/bin/bash
 echo -e "What file do you want to compile & run, no extension please?" 
 read file
 source="src/${file}.c"
-destination="output/${file}"
+destination="output/${file}.out"
 gcc-10 $source -o $destination -O3 -g -Wall -std=c17 -Wno-missing-braces
 gdb $destination
